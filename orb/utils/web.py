d68c8045af5c9b5e8159db440d675ed7dcca7856
Barthélémy Launet <barthelemy.launet@obspm.fr>
2018-01-17 13:10:32 +0100

    Ability to query the PHAT catalog. Not so useful

--- orb/utils/web.py
+++ orb/utils/web.py
@@ -4,7 +4,7 @@
 # File: image.py
 
 ## Copyright (c) 2010-2017 Thomas Martin <thomas.martin.1@ulaval.ca>
-## 
+##
 ## This file is part of ORB
 ##
 ## ORB is free software: you can redistribute it and/or modify it
@@ -32,7 +32,7 @@ import warnings
 def query_sesame(object_name, verbose=True, degree=False, pm=False):
     """Query the SESAME Database to get RA/DEC given the name of an
     object.
-    
+
     :param object_name: Name of the object
 
     :param verbose: (Optional) If True print messages (default True)
@@ -42,7 +42,7 @@ def query_sesame(object_name, verbose=True, degree=False, pm=False):
 
     :param pm: (Optional) If True proper motion is also returned
       (default False)
-    
+
     :returns: [RA, DEC]
     """
     if verbose:
@@ -86,15 +86,15 @@ def query_sesame(object_name, verbose=True, degree=False, pm=False):
     else:
         pm_ra = None
         pm_dec = None
-        
-    
+
+
     if verbose:
         logging.info("RA: {}".format(ra))
         logging.info("DEC: {}".format(dec))
         if pm:
             logging.info("PM RA: {}".format(pm_ra))
             logging.info("PM DEC: {}".format(pm_dec))
-        
+
 
     if not degree:
         ra = ra.split(":")
@@ -107,7 +107,7 @@ def query_sesame(object_name, verbose=True, degree=False, pm=False):
 
 
 def query_vizier(radius, target_ra, target_dec,
-                 catalog='gaia', max_stars=100, return_all_columns=False):
+                 catalog='gaia', max_stars=100, return_all_columns=False, timeout=5):
     """Return a list of star coordinates around an object in a
     given radius based on a query to VizieR Services
     (http://vizier.u-strasbg.fr/viz-bin/VizieR)
@@ -132,7 +132,7 @@ def query_vizier(radius, target_ra, target_dec,
     :param return_all_columns: (Optional) If True, return all
       columns. Else only ra, dec and Mag are returned (default False).
     """
-    MAX_RETRY = 5
+    MAX_RETRY = 2
     if catalog == 'usno':
         catalog_id = 'USNO-B1.0'
         out = '_RAJ2000,_DEJ2000,e_RAJ2000,e_DEJ2000,R2mag'
@@ -145,6 +145,10 @@ def query_vizier(radius, target_ra, target_dec,
         catalog_id = 'II/246/out'
         out = 'RAJ2000,DEJ2000,errMaj,errMin,Jmag,e_Jmag,Hmag,e_Hmag,Kmag,e_Kmag'
         sort='-Jmag'
+    elif catalog == 'phat':
+        catalog_id = 'J/ApJS/215/9/table5'
+        out = 'RAJ2000,DEJ2000,F475W'
+        sort='-F475W'
 
     else: raise Exception("Bad catalog name. Can be 'usno', 'gaia' or '2mass'")
 
@@ -153,16 +157,19 @@ def query_vizier(radius, target_ra, target_dec,
     logging.info("Sending query to VizieR server (catalog: {})".format(catalog))
     logging.info("Looking for stars at RA: %f DEC: %f"%(target_ra, target_dec))
 
-    URL = (orb.constants.VIZIER_URL + "asu-tsv/?-source=" + catalog
+    URL = (orb.constants.VIZIER_URL + "asu-tsv/?-source=" + catalog_id
            + "&-c.ra=%f"%target_ra + '&-c.dec=%f'%target_dec
            + "&-c.rm=%d"%int(radius)
-           + '&-out.max=unlimited&-out.meta=-huD'
+           + '&-out.max=%d'%int(max_stars*2)
+           + '&-out.meta=-huD'
            + '&-out={}&-sort={}'.format(out, sort))
+
+    logging.debug("URL sent : %s"%URL)
 
     retry = 0
     while retry <= MAX_RETRY:
         try:
-            query_result = urllib2.urlopen(URL, timeout=5)
+            query_result = urllib2.urlopen(URL, timeout=timeout)
             break
 
         except urllib2.URLError:
@@ -187,19 +194,22 @@ def query_vizier(radius, target_ra, target_dec,
         if iline[0] != '#' and iline[0] != '-' and len(iline) > 3:
             iline = iline.split()
             if len(iline) == params_number:
-                if ((float(iline[2])/1000.) < 0.5
-                    and (float(iline[3])/1000.) < 0.5):
-                    if not return_all_columns:
-                        star_list.append((float(iline[0]),
-                                          float(iline[1]),
-                                          float(iline[4])))
-                    else:
-                        star_list.append(
-                            list((float(iline[0]),
-                                  float(iline[1]),
-                                  float(iline[4])))
-                            + list(np.array(iline[5:], dtype=float)))
-                        
+                if catalog == 'phat':
+                    star_list.append(map(float, iline))
+                else:
+                    if ((float(iline[2])/1000.) < 0.5
+                        and (float(iline[3])/1000.) < 0.5):
+                        if not return_all_columns:
+                            star_list.append((float(iline[0]),
+                                              float(iline[1]),
+                                              float(iline[4])))
+                        else:
+                            star_list.append(
+                                list((float(iline[0]),
+                                      float(iline[1]),
+                                      float(iline[4])))
+                                + list(np.array(iline[5:], dtype=float)))
+
 
     # sorting list to get the brightest stars first
     star_list = np.array(sorted(star_list, key=lambda istar: istar[2]))
